77e603989a4e2f21eb810268a07c2d8bb62e7256
a-nester <a_nester@ukr.net>
2026-02-16 23:07:16 +0200

    Update BASE_URL from remote server to localhost.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://remoteorder-server.onrender.com";
+// export const BASE_URL = "https://remoteorder-server.onrender.com";
+export const BASE_URL = "http://localhost:3000";
 export const API_URL = `${BASE_URL}/api`;
 // export const API_URL = "http://localhost:3000/api";
 // export const ADMIN_SECRET = "secure-admin-key-123"; // REMOVED FOR SECURITY
