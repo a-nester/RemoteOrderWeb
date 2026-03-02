a91e867ad06e72841bdf061dd10a1daaf064f1aa
a-nester <a_nester@ukr.net>
2026-03-02 20:43:43 +0200

    fix-api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://remoteorder-server.onrender.com";
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "https://remoteorder-server.onrender.com";
+// export const BASE_URL = "http://localhost:5001";
 export const API_URL = `${BASE_URL}/api`;
 // export const API_URL = "http://localhost:3000/api";
 // export const ADMIN_SECRET = "secure-admin-key-123"; // REMOVED FOR SECURITY
