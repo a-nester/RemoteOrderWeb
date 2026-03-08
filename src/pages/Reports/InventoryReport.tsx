bf7d278afd182abf0cc7f07f9c900243359e7093
a-nester <a_nester@ukr.net>
2026-03-08 21:17:39 +0200

    fix-inventoryReport

--- src/pages/Reports/InventoryReport.tsx
+++ src/pages/Reports/InventoryReport.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, Fragment } from "react";
+import { useState, useEffect, useMemo, Fragment } from "react";
 import { useTranslation } from "react-i18next";
 import {
   Search,
